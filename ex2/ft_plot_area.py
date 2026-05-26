5b974ed55db2237d517e57fa0548dc0966eeb249
erraii <eraycakiray@gmail.com>
2026-05-26 11:23:43 +0200

    Create ft_plot_area.py

---
+++ ex2/ft_plot_area.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+def ft_plot_area() -> None:
+    length = int(input("Enter length: "))
+    width = int(input("Enter width: "))
+    print("Plot area: " + str(length * width))
